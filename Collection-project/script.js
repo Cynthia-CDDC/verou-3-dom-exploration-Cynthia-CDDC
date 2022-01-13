f41883b240b44d6544cafdfe123bcaabbf237b18
CDDC <cynthia.decelle@gmail.com>
2022-01-13 11:46:58 +0100

    Fixe photos not showing.

--- Collection-project/script.js
+++ Collection-project/script.js
@@ -25,14 +25,14 @@ const bookCollection = [
         "But how can one trade with the Rain Wilders, when only a liveship fashioned from wizardwood can negotiate the perilous waters of the Rain River? Rare and valuable a liveship will quicken only when three members, from successive generations, have died on board. The liveship Vivacia is about to undergo her quickening as Althea Vestrit's father is carried on deck in his death-throes. Althea waits for the ship that she loves more than anything else in the world to awaken. Only to discover that the Vivacia has been signed away in her father’s will to her brutal brother-in-law, Kyle Haven..."+
         
         "Others plot to win or steal a liveship. The Paragon, known by many as the Pariah, went mad, turned turtle, and drowned his crew. Now he lies blind, lonely, and broken on a deserted beach. But greedy men have designs to restore him, to sail the waters of the Rain Wild River once more.",
-        cover:"./",
+        cover:"./photos/Ship-of-Magic.jpg",
     },
     {
         author:"Ricardo Semler",
         genre:"Memoir",
         title:"Maverick!",
         synopsis:"Semler turned his family's business, the aging Semco corporation of Brazil, into the most revolutionary business success story of our time. By eliminating uneeded layers of management and allowing employees unprecedented democracy in the workplace, he created a company that challenged the old ways and blazed a path to success in an uncertain economy.",
-        cover:"./",
+        cover:"./photos/Maverick!.jpg",
     },
     {
         author:"Gail Carriger",
@@ -43,7 +43,7 @@ const bookCollection = [
         "Fourteen-year-old Sophronia is a great trial to her poor mother. Sophronia is more interested in dismantling clocks and climbing trees than proper manners and the family can only hope that company never sees her atrocious curtsy. Mrs. Temminnick is desperate for her daughter to become a proper lady. So she enrolls Sophronia in Mademoiselle Geraldine's Finishing Academy for Young Ladies of Quality."+
         
         "But Sophronia soon realizes the school is not quite what her mother might have hoped. At Mademoiselle Geraldine's, young ladies learn to finish…everything. Certainly, they learn the fine arts of dance, dress, and etiquette, but the also learn to deal out death, diversion, and espionage in the politest possible ways, of course. Sophronia and her friends are in for a rousing first year's education.",
-        cover:"./",
+        cover:"./photos/Etiquette-and-Espionage.jpg",
     },
     {
         author:"Stephenie Meyer",
@@ -53,14 +53,14 @@ const bookCollection = [
 
         "When Melanie, one of the few remaining “wild” humans is captured, she is certain it is her end. Wanderer, the invading “soul” who has been given Melanie's body, was warned about the challenges of living inside a human: the overwhelming emotions, the glut of senses, the too vivid memories. But there was one difficulty Wanderer didn't expect: the former tenant of her body refusing to relinquish possession of her mind."+
         "Wanderer probes Melanie's thoughts, hoping to discover the whereabouts of the remaining human resistance. Instead, Melanie fills Wanderer's mind with visions of the man Melanie loves—Jared, a human who still lives in hiding. Unable to separate herself from her body's desires, Wanderer begins to yearn for a man she has been tasked with exposing. When outside forces make Wanderer and Melanie unwilling allies, they set off on a dangerous and uncertain search for the man they both love.",
-        cover:"./",
+        cover:"./photos/the host.jpg",
     },
     {
         author:"Antoine De Saint Exupéry",
         genre:"Tale",
         title:"The Little Prince",
         synopsis:"A pilot crashes in the Sahara Desert and encounters a strange young boy who calls himself the Little Prince. The Little Prince has traveled there from his home on a lonely, distant asteroid with a single rose. The story that follows is a beautiful and at times heartbreaking meditation on human nature.",
-        cover:"./",
+        cover:"./photos/The-Little-Prince.jpg",
     },
 ];
 console.log(bookCollection);
@@ -74,7 +74,7 @@ for (let book of bookCollection) {
 
     const newImage = document.createElement("img");
     newImage.className = "photo";
-    newImage.src = "cover";
+    newImage.src = book.cover;
     newSection.appendChild(newImage)
 
     const newDiv = document.createElement("div");
